66b3c245c751c0b0881529f9ca0618ee90dc3e07
Artur Lobanov <artur.lobanov@desy.de>
2014-09-24 18:02:39 +0200

    adding test run script for NAF

---
+++ CMGTools/TTHAnalysis/cfg/run_test_naf_cfg.py
@@ -0,0 +1,78 @@
+##########################################################
+##       CONFIGURATION FOR SUSY MULTILEPTON TREES       ##
+## skim condition: >= 2 loose leptons, no pt cuts or id ##
+##########################################################
+
+import CMGTools.RootTools.fwlite.Config as cfg
+from CMGTools.RootTools.fwlite.Config import printComps
+from CMGTools.RootTools.RootTools import *
+
+#Load all analyzers
+from CMGTools.TTHAnalysis.analyzers.susyCore_modules_cff import *
+
+# Redefine what I need
+
+# --- LEPTON SKIMMING ---
+ttHLepSkim.minLeptons = 1
+ttHLepSkim.maxLeptons = 999
+#ttHLepSkim.idCut  = ""
+#ttHLepSkim.ptCuts = []
+
+
+# Event Analyzer for susy multi-lepton (at the moment, it's the TTH one)
+ttHEventAna = cfg.Analyzer(
+    'ttHLepEventAnalyzer',
+    minJets25 = 0,
+    )
+
+
+
+
+# Tree Producer
+treeProducer = cfg.Analyzer(
+    'treeProducerSusySingleLepton',
+    vectorTree = True,
+    saveTLorentzVectors = False,  # can set to True to get also the TLorentzVectors, but trees will be bigger
+    PDFWeights = PDFWeights,
+
+    )
+
+
+#-------- SAMPLES AND TRIGGERS -----------
+from CMGTools.TTHAnalysis.samples.samples_13TeV_CSA14_desy import *
+selectedComponents = [ TTJets_PU40bx25_V52 ]
+
+
+#-------- SEQUENCE
+
+sequence = cfg.Sequence(susyCoreSequence+[
+    ttHEventAna,
+    treeProducer,
+    ])
+
+
+#-------- HOW TO RUN
+test = 3
+if test==1:
+    # test a single component, using a single thread.
+    comp = TTJets_PU40bx25_V52
+    comp.files = comp.files[:1]
+    selectedComponents = [comp]
+    comp.splitFactor = 1
+elif test==2:
+    # test all components (1 thread per component).
+    for comp in selectedComponents:
+        comp.splitFactor = 1
+        comp.files = comp.files[:1]
+elif test==3:
+#    # test a single component, using a single thread.
+    comp = TTJets_PU40bx25_V52
+#    comp.files = comp.files[:10]
+    selectedComponents = [comp]
+    comp.splitFactor = len(comp.files)
+
+
+config = cfg.Config( components = selectedComponents,
+                     sequence = sequence )
+
+printComps(config.components, True)
